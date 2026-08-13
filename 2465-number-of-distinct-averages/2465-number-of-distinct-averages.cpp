3da17f031ae88eae4ec132bf6f846693feb01886
Swaraj_Prajapati <sabitacbsa108@gmail.com>
2026-08-13 16:58:03 +0530

    Time: 0 ms (100%), Space: 9.4 MB (83.86%) - LeetHub

---
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int distinctAverages(vector<int>& nums) {
+        set<double>s1;
+
+        sort(nums.begin(),nums.end());
+        int i = 0;
+
+        int  j =  nums.size()-1;
+
+        while(i<j){
+           double avg  =  (nums[i]+nums[j])/2.0; 
+           s1.insert(avg);
+
+           i++;
+           j--;         
+        }
+        return s1.size();
+    }
+};
