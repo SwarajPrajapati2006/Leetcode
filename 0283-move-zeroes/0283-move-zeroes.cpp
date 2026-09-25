287b06f8a9657853da2ce1e35d36e27f39125116
Swaraj_Prajapati <sabitacbsa108@gmail.com>
2026-09-25 22:18:25 +0530

    Time: 3 ms (14.41%), Space: 26.3 MB (5.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
-        int k = 0;
-        for(int i = 0;i<nums.size();i++){
-             if(nums[i]!=0){
-                nums[k]  = nums[i];
-                k++;
+        vector<int>zero;
+        vector<int>non;
+        vector<int>res;
 
-             }
+        // int k = 0;
+        // for(int i = 0;i<nums.size();i++){
+        //      if(nums[i]!=0){
+        //         nums[k]  = nums[i];
+        //         k++;
 
-        } 
-         while(k<nums.size()){
-            nums[k] = 0;
-            k++;
-         }
+        //      }
 
+        // } 
+        //  while(k<nums.size()){
+        //     nums[k] = 0;
+        //     k++;
+        //  }
+            for(int i = 0 ;i<nums.size();i++){
+                if(nums[i]!=0){
+                    non.push_back(nums[i]);
+                }
+                else{
+                    zero.push_back(nums[i]);
+                }
+            }
+
+            for(int i = 0 ;i<non.size();i++){
+                res.push_back(non[i]);
+            }
+            for(int i = 0 ;i<zero.size();i++){
+                res.push_back(zero[i]);
+            }
+            nums = res;
     }
 };
